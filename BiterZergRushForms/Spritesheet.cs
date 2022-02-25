be2a91d029cfe81b971f70787eb9627283b9577f
Daniel <danielweper@gmail.com>
2022-02-25 09:41:11 +0100

    Made a new (and more generalised) Spritesheet class

---
+++ BiterZergRushForms/Spritesheet.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace BiterZergRushForms
+{
+    public class Spritesheet
+    {
+        private readonly Bitmap[] sprites;
+
+        public Spritesheet(Bitmap spritesheetImage, int spriteWidth, int spriteHeight, int spriteCount)
+        {
+            sprites = new Bitmap[spriteCount];
+
+            int columnCount = spritesheetImage.Width / spriteWidth;
+            for (int i = 0; i < spriteCount; i++)
+            {
+                int row = Math.DivRem(i, columnCount, out int column);
+                Rectangle spriteArea = new Rectangle(column * spriteWidth, row * spriteHeight, spriteWidth, spriteHeight);
+
+                Bitmap target = new Bitmap(spriteWidth, spriteHeight);
+
+                using (Graphics g = Graphics.FromImage(target))
+                {
+                    g.DrawImage(
+                        spritesheetImage,
+                        new Rectangle(0, 0, spriteWidth, spriteHeight),
+                        spriteArea,
+                        GraphicsUnit.Pixel);
+                }
+
+                sprites[i] = target;
+            }
+        }
+
+        public static Spritesheet MakeSpritesheetFromSpriteDimensions(Bitmap spritesheetImage, int spriteWidth, int spriteHeight)
+        {
+            int columnCount = spriteWidth / spritesheetImage.Width;
+            int rowCount = spriteHeight / spritesheetImage.Height;
+
+            return new Spritesheet(spritesheetImage, spriteWidth, spriteHeight, rowCount * columnCount);
+        }
+
+        public static Spritesheet MakeSpritesheetFromSpriteCount(Bitmap spritesheetImage, int columnCount, int rowCount)
+        {
+            int spriteWidth = spritesheetImage.Width / columnCount;
+            int spriteHeight = spritesheetImage.Height / rowCount;
+
+            return new Spritesheet(spritesheetImage, spriteWidth, spriteHeight, rowCount * columnCount);
+        }
+
+        public int Length => sprites.Length;
+
+        public Bitmap this[int index]
+        {
+            get
+            {
+                return sprites[index];
+            }
+        }
+
+        public IEnumerable<Bitmap> T()
+        {
+            foreach (Bitmap sprite in sprites)
+            {
+                yield return sprite;
+            }
+        }
+    }
+}
